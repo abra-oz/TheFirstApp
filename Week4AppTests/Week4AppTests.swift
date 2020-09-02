286364d12b6789b74d39aa406ab199246a726fe5
Abra <aolvera@butlercc.edu>
2020-09-02 12:34:55 -0500

    Initial Commit

---
+++ Week4AppTests/Week4AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Week4AppTests.swift
+//  Week4AppTests
+//
+//  Created by Student on 9/2/20.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import XCTest
+@testable import Week4App
+
+class Week4AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
